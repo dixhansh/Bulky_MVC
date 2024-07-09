37879e3c361be6954226ca826735e5f65eb4b043
dixhansh <dixhansh@gmail.com>
2024-07-09 13:31:04 +0530

    Added CRUD operations

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -21,5 +21,78 @@ namespace BulkyWeb.Controllers
 
             return View(categories); 
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {       //adding error message for a real world business scenario(following check has no significance)
+            if (obj.Name == obj.DisplayOrder.ToString())//toString() will convert the numeric value to string type
+
+            {   //ModelState is a property of Controller that contains all the data posted by the user(here ModelState is an object of Category class)
+                ModelState.AddModelError("name", "Name and displayOrder can't be same");//here we are adding a error message to "Name" property of ModelState   
+            }
+
+            if(obj.Name == "test")
+            {//Model level is set because of ""
+                    ModelState.AddModelError("", "This will be set as a model level error since we did not specified any property name like we did in above example");
+            }
+
+            if (ModelState.IsValid)//this will check all the validations applied using data annotations in Model Class
+            {
+                _db.categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id) //? allows this parameter to be a null value
+        {
+            if(id ==null || id <= 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.categories.Find(id); //Note that Find() only works with the primary key
+
+          /*  //Searching in a non primary key property
+            Category? categoryFromDb1 = _db.categories.FirstOrDefault(cat => cat.Id==id);//here i could even iterate over a non-PK property and this will                                                                           return the first satisfying the condition or default if source is empty
+            
+            //This is another way if property is Non-PK. Here we can apply a filter with Where clause.
+            Category? categoryFromDb2 = _db.categories.Where(cat => cat.Id == id).FirstOrDefault();*/
+
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+         public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            Category? categoryToDel = _db.categories.Find(id);
+            _db.categories.Remove(categoryToDel);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
